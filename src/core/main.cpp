a9bff7a267f7f590dec854c95a7828a5832e610d
charruau <echarruau@gmail.com>
2016-07-27 17:02:14 +0200

    first attempt to add network but can not find where object client from network chat is created

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -30,6 +30,10 @@
 #include <QPixmap>
 #include <QSettings>
 
+#include <QtCore/QSettings>
+#include <QtNetwork/QNetworkConfigurationManager>
+#include <QtNetwork/QNetworkSession>
+
 #include "ApplicationInfo.h"
 #include "ActivityInfoTree.h"
 #include "File.h"
@@ -103,6 +107,18 @@ QString loadTranslation(QSettings &config, QTranslator &translator)
     return locale;
 }
 
+
+void appendMessage(const QString &from, const QString &message)
+{
+    if (from.isEmpty() || message.isEmpty())
+        return;
+
+    qDebug() << "GCNetwork: " << message;
+}
+
+
+
+
 int main(int argc, char *argv[])
 {
     // Disable it because we already support HDPI display natively
@@ -113,6 +129,48 @@ int main(int argc, char *argv[])
     app.setApplicationName(GCOMPRIS_APPLICATION_NAME);
     app.setOrganizationDomain("kde.org");
     app.setApplicationVersion(ApplicationInfo::GCVersion());
+
+
+    QNetworkConfigurationManager manager;
+    if (manager.capabilities() & QNetworkConfigurationManager::NetworkSessionRequired) {
+        // Get saved network configuration
+        QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+        settings.beginGroup(QLatin1String("QtNetwork"));
+        const QString id = settings.value(QLatin1String("DefaultNetworkConfiguration")).toString();
+        settings.endGroup();
+
+        // If the saved network configuration is not currently discovered use the system default
+        QNetworkConfiguration config = manager.configurationFromIdentifier(id);
+        if ((config.state() & QNetworkConfiguration::Discovered) !=
+            QNetworkConfiguration::Discovered) {
+            config = manager.defaultConfiguration();
+        }
+
+        QNetworkSession *networkSession = new QNetworkSession(config, &app);
+        networkSession->open();
+        networkSession->waitForOpened();
+
+        if (networkSession->isOpen()) {
+            // Save the used configuration
+            QNetworkConfiguration config = networkSession->configuration();
+            QString id;
+            if (config.type() == QNetworkConfiguration::UserChoice) {
+                id = networkSession->sessionProperty(
+                        QLatin1String("UserChoiceConfiguration")).toString();
+            } else {
+                id = config.identifier();
+            }
+
+            QSettings settings(QSettings::UserScope, QLatin1String("QtProject"));
+            settings.beginGroup(QLatin1String("QtNetwork"));
+            settings.setValue(QLatin1String("DefaultNetworkConfiguration"), id);
+            settings.endGroup();
+        }
+    }
+
+    connect(&client, SIGNAL(newMessage(QString,QString)),this, SLOT(appendMessage(QString,QString)));
+
+
 
 #if defined(Q_OS_MAC)
     // Sandboxing on MacOSX as documented in:
